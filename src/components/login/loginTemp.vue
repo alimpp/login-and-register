a84b2176bbfc0152ebd1219346589db4d1ea685c
ali.mpp <alimpp766@gmail.com>
2022-05-28 14:35:20 +0430

    make login and register

--- src/components/login/loginTemp.vue
+++ src/components/login/loginTemp.vue
@@ -1,15 +1,92 @@
 <template>
   <div class="loginTemp">
-  login
+     <div class="textBox">
+         <div class="glass p-5">
+         <p class="text_25 opacityAnim light_color">Welcome To MY Website</p>
+         <p class="text_18 opacityAnim light_color">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quas ad maxime architecto ipsam expedita omnis harum consequuntur earum eveniet, culpa possimus doloribus reiciendis quam dolorum magnam provident natus in</p>
+           <div class="d-flex opacityAnim">
+           <i class="bi bi-instagram light_color mx-2" style="font-size:25px;"></i>
+          <i class="bi bi-linkedin light_color mx-2" style="font-size:25px;"></i>
+          <i class="bi bi-twitter light_color mx-2" style="font-size:25px;"></i>
+          <i class="bi bi-facebook light_color mx-2" style="font-size:25px;"></i>
+          </div>
+         </div>
+     </div>
+     <div class="formBox p-4 opacityAnim">
+         <p class="text_25">Login With News</p>
+         <p class="text_18">Email</p>
+         <input type="email" class="form-control" v-model="email">
+         <p class="text_18 danger_color" v-if="emailRequired">{{emailRequiredText}}</p>
+         <p class="text_18">Password</p>
+         <input type="password" class="form-control" v-model="password">
+        <p class="text_18 danger_color" v-if="passwordRequired">{{passwordRequiredText}}</p>
+         <div class="d-flex">
+         <button class="btn btn-primary mt-2" @click="login">Login in</button>
+         <div class="d-flex mt-3 mx-2">
+         <router-link to="/register">Do you have account ?</router-link>
+         </div>
+         </div>
+     </div>
   </div>
 </template>
 
 <script>
 export default {
-
+    data(){
+      return{
+        email : "" , 
+        password : "" , 
+        emailRequired : false ,
+        emailRequiredText : "Email is Required" , 
+        passwordRequired : false , 
+        passwordRequiredText : "Password is Required"
+      }
+    } , 
+    methods : {
+      login(){
+        if(this.email === "" && this.password === ""){
+          this.emailRequired = true
+          this.passwordRequired = true
+        }else if(this.email === ""){
+            this.emailRequired = true
+        } else if(this.email.length < 5){ 
+            this.emailRequired = true
+        } else if(this.password === ""){
+          this.passwordRequired = true
+        } else if(this.password.length < 5){
+          this.passwordRequired = true
+        } else{
+          this.emailRequired = false 
+          this.passwordRequired = false
+          this.$router.push("/news")
+        }
+      }
+    }
 }
 </script>
 
-<style>
-
+<style scoped >
+.loginTemp{
+  width: 100%;
+  height: 100vh;
+  display: flex;
+}
+.textBox{
+  width: 60%;
+  height: 100vh;
+  background: url(./paradise.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.formBox{
+   width: 60%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+@media (max-width: 800px) {
+  .textBox{display: none;}
+  .formBox{width: 100%;}
+}
 </style>
--- src/components/register/registerTemp.vue
+++ src/components/register/registerTemp.vue
@@ -1,15 +1,104 @@
 <template>
   <div class="registerTemp">
-  register
+     <div class="textBox">
+         <div class="glass p-5">
+         <p class="text_25 opacityAnim light_color">Welcome To MY Website</p>
+         <p class="text_18 opacityAnim light_color">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quas ad maxime architecto ipsam expedita omnis harum consequuntur earum eveniet, culpa possimus doloribus reiciendis quam dolorum magnam provident natus in</p>
+           <div class="d-flex opacityAnim">
+           <i class="bi bi-instagram light_color mx-2" style="font-size:25px;"></i>
+          <i class="bi bi-linkedin light_color mx-2" style="font-size:25px;"></i>
+          <i class="bi bi-twitter light_color mx-2" style="font-size:25px;"></i>
+          <i class="bi bi-facebook light_color mx-2" style="font-size:25px;"></i>
+          </div>
+         </div>
+     </div>
+     <div class="formBox p-4 opacityAnim">
+         <p class="text_25">Register With News</p>
+         <p class="text_18">Email</p>
+         <input type="email" class="form-control" v-model="email">
+         <p class="text_18 danger_color" v-if="emailRequired">{{emailRequiredText}}</p>
+         <p class="text_18">Password</p>
+         <input type="" class="form-control" v-model="password">
+        <p class="text_18 danger_color" v-if="passwordRequired">{{passwordRequiredText}}</p>
+        <p class="text_18">Confrim Password</p>
+        <input type="" class="form-control" v-model="confrimPassword">
+        <p class="text_18 danger_color" v-if="passwordRequired">{{confrimPasswordRequiredText}}</p>
+         <div class="d-flex">
+         <button class="btn btn-primary mt-2" @click="register">Register</button>
+         <router-link to="/login" class="btn btn-warning mt-2 mx-2">Login</router-link>
+         <div class="d-flex" style="margin:18px 10px;">
+         </div>
+         </div>
+     </div>
   </div>
 </template>
 
 <script>
 export default {
-
+    data(){
+      return{
+        email : "" , 
+        password : "" , 
+        confrimPassword : "" ,
+        emailRequired : false ,
+        emailRequiredText : "Email is Required" , 
+        passwordRequired : false , 
+        passwordRequiredText : "Password is Required" , 
+        confrimPasswordRequired : false ,
+        confrimPasswordRequiredText : "Confrim Password is Required"
+      }
+    } , 
+    methods : {
+      register(){
+        if(this.email === "" && this.password === ""){
+          this.emailRequired = true
+          this.passwordRequired = true
+        }else if(this.email === ""){
+            this.emailRequired = true
+        } else if(this.email.length < 5){ 
+            this.emailRequired = true
+        } else if(this.password === ""){
+          this.passwordRequired = true
+        } else if(this.password.length < 5){
+          this.passwordRequired = true
+        } else if(this.confrimPassword === ""){
+           this.confrimPasswordRequired = true
+        } else if(this.confrimPassword === this.password){
+          this.passwordRequired = true 
+          this.confrimPasswordRequired = true
+        } else{
+          this.emailRequired = false 
+          this.passwordRequired = false 
+          this.confrimPasswordRequired = false 
+          this.$router.push("/news")
+        }
+      }
+    }
 }
 </script>
 
-<style>
-
+<style scoped >
+.registerTemp{
+  width: 100%;
+  height: 100vh;
+  display: flex;
+}
+.textBox{
+  width: 60%;
+  height: 100vh;
+  background: url(./paradise.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.formBox{
+   width: 60%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+@media (max-width: 800px) {
+  .textBox{display: none;}
+  .formBox{width: 100%;}
+}
 </style>
